47e8034d13fb78b368f565f223999200700d97d4
Yassar Hammami <yassarhammami@gmail.com>
2022-07-27 23:00:18 +0300

    multithreading

--- src/main/java/com/jazara/icu/consumecam/ConsumeCamApplication.java
+++ src/main/java/com/jazara/icu/consumecam/ConsumeCamApplication.java
@@ -1,15 +1,23 @@
 package com.jazara.icu.consumecam;
 
+import nu.pattern.OpenCV;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableDiscoveryClient
+@EnableScheduling
 @SpringBootApplication
-public class ConsumeCamApplication {
+public class ConsumeCamApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumeCamApplication.class, args);
     }
 
+    @Override
+    public void run(String... strings) throws Exception {
+        OpenCV.loadShared();
+    }
 }
--- src/main/java/com/jazara/icu/consumecam/service/ConsumeCamService.java
+++ src/main/java/com/jazara/icu/consumecam/service/ConsumeCamService.java
@@ -1,15 +1,31 @@
 package com.jazara.icu.consumecam.service;
 
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ConsumeCamService {
 
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private Long i = 0L;
 
-    public void consumeCam(String url) {
+    @Async
+    @Scheduled(fixedRate = 1000L)
+    public CompletableFuture<?> consumeCam(String url) {
         LOGGER.info("Running AI Instance for streaming : " + url);
+
+        VideoCapture capture = new VideoCapture(url);
+        Mat mat = new Mat();
+        capture.read(mat);
+        //send mat to ai backend and handle response
+        LOGGER.info("Done " + this.i++ + " frame for : " + url);
+        return CompletableFuture.completedFuture(null);
     }
 }
