0db7da35e7494a08bc0e52ddabee2c739b2f2da3
Yassar Hammami <yassarhammami@gmail.com>
2022-07-28 04:34:29 +0300

    Update CamDTO.java

--- src/main/java/com/jazara/icu/consumecam/domain/CamDTO.java
+++ src/main/java/com/jazara/icu/consumecam/domain/CamDTO.java
@@ -1,5 +1,6 @@
 package com.jazara.icu.consumecam.domain;
 
+import java.util.List;
 import java.util.Objects;
 
 public class CamDTO {
@@ -7,6 +8,7 @@ public class CamDTO {
     private Long room_id;
     private String name;
     private String url;
+    private List<Long> persons;
 
     public String getId() {
         return id;
@@ -40,14 +42,23 @@ public class CamDTO {
         this.url = url;
     }
 
+    public List<Long> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Long> persons) {
+        this.persons = persons;
+    }
+
     public CamDTO() {
     }
 
-    public CamDTO(String id, Long room_id, String name, String url) {
+    public CamDTO(String id, Long room_id, String name, String url, List<Long> persons) {
         this.id = id;
         this.room_id = room_id;
         this.name = name;
         this.url = url;
+        this.persons = persons;
     }
 
     @Override
@@ -58,11 +69,12 @@ public class CamDTO {
         return Objects.equals(id, camDTO.id) &&
                 Objects.equals(room_id, camDTO.room_id) &&
                 Objects.equals(name, camDTO.name) &&
-                Objects.equals(url, camDTO.url);
+                Objects.equals(url, camDTO.url) &&
+                Objects.equals(persons, camDTO.persons);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, room_id, name, url);
+        return Objects.hash(id, room_id, name, url, persons);
     }
 }
