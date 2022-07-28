e72e233c5739cd2f8f1432adee91598a5a2828f1
Yassar Hammami <yassarhammami@gmail.com>
2022-07-28 03:54:12 +0300

    .

--- src/main/java/com/jazara/icu/consumecam/ConsumeCamApplication.java
+++ src/main/java/com/jazara/icu/consumecam/ConsumeCamApplication.java
@@ -4,6 +4,7 @@ import nu.pattern.OpenCV;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,7 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableCircuitBreaker
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ConsumeCamApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
